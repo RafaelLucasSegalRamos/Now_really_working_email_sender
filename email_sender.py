5a7f38427d46047d039b7f3fdba289ab59d14bbf
Rafael <94694500+RafaelLucasSegalRamos@users.noreply.github.com>
2022-08-23 21:41:29 -0300

    mais um teste que não funcionou

--- email_sender.py
+++ email_sender.py
@@ -1,8 +1,9 @@
 #! C:\Users\rafae\AppData\Local\Programs\Python\Python310\python.exe
 
 import cgi, os
-
+import win32com.client as win32
 print('content-type:text/html\r\n\r\n')
+outlook = win32.Dispatch('outlook.application')
 
 form =  cgi.FieldStorage()
 nome = str(form.getvalue("pname"))
@@ -11,26 +12,25 @@ Text = str(form.getvalue("des"))
 File = form['filename']
 
 fn = os.path.basename(File.filename)
-open("C:/xampp/htdocs/Now_really_working_email_sender/tem/"+fn, "wb").write(File.file.read())
 
-print("""<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Email sender</title>
-</head>
-<body>"""
-)
-print("""<h1> New Message \n(%s)</h1>"""%nome)
+titulo = f'Ola meu nome é {nome}'
 
-print("""<img scr="tem/%s">"""%fn)
-print("<h2>%s</h2>"%Text)      
-
-print("""
-      </body>
-</html> 
-""")      
+email = outlook.CreateItem(0)
 
 
+email.To = "mamacosupremo.sol@gmail.com"
+email.Subject = titulo
+email.HTMLbody = (f"""
+                    <h1> Olá Rafael!</h1>
+                    
+                    <p>{Text}</p>
+                    
+                    <p> E aqui uma pequena supresa: </p>
+                    
+                    <a href="{fn}"><img scr="fn"></a>
+                    
+                    <h2> Obrigado por ter lido minha menssagem</h2>
+                    <h1> De: {nome}</h1>
+                    """)
+email.Send()
+print("Email Enviado")   

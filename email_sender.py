73b80e03adedf853fa47c26915c2c35a1fcf64b3
Rafael <94694500+RafaelLucasSegalRamos@users.noreply.github.com>
2022-08-25 19:57:39 -0300

    update

--- email_sender.py
+++ email_sender.py
@@ -1,25 +1,33 @@
 #! C:\Users\rafae\AppData\Local\Programs\Python\Python310\python.exe
 
 import cgi, os
+from ctypes import addressof
+import cgitb
 import win32com.client as win32
 print('content-type:text/html\r\n\r\n')
 outlook = win32.Dispatch('outlook.application')
 
+
+cgitb.enable( )
 form =  cgi.FieldStorage()
-nome = str(form.getvalue("Nome"))
+nome = str(form.getvalue("pname"))
 Text = str(form.getvalue("text"))
 
-File = form['arquivo']
+file = form["filename"]
 
-fn = os.path.basename(File.filename)
+filenames = os.path.basename(file.filename)
+open("C:/xampp/htdocs/Now_really_working_email_sender/tem/"+ filenames, "wb").write(file.file.read())
 
-titulo = f'Ola meu nome é {nome}'
+email = outlook.CreateItem(0)
 
-# email = outlook.CreateItem(0)
-
-
-print("<html><head><title>Titulo1</title></head>")
-print(f"<body><h1>{nome}</h1>")
-print(f"<img scr='{fn}'>")
-print(f"<p>{Text}</p>")
-print("</body></html>")
+email.To = "mamacosupremo.sol@gmail.com"
+email.Subject = f"Olá meu nome é {nome}"
+email.HTMLbody = (f"""
+                    <h1 style="background: #222; padding: 20px; border-radius: 20px;"> Olá Rafael!</h1>
+                    
+                    <p>{Text}</p>
+                    
+                    <h2> De: </h2> <h1>{nome}</h1>
+                    """)
+email.Send()
+print("Email Enviado")

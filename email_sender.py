3a8cec50d9ed021474c6094a789f105bbb3de63f
Rafael <94694500+RafaelLucasSegalRamos@users.noreply.github.com>
2022-08-25 20:30:57 -0300

    update

--- email_sender.py
+++ email_sender.py
@@ -1,14 +1,13 @@
 #! C:\Users\rafae\AppData\Local\Programs\Python\Python310\python.exe
 
 import cgi, os
-from ctypes import addressof
 import cgitb
 import win32com.client as win32
 print('content-type:text/html\r\n\r\n')
 outlook = win32.Dispatch('outlook.application')
+email = outlook.CreateItem(0)
 
-
-cgitb.enable( )
+cgitb.enable()
 form =  cgi.FieldStorage()
 nome = str(form.getvalue("pname"))
 Text = str(form.getvalue("text"))
@@ -18,16 +17,16 @@ file = form["filename"]
 filenames = os.path.basename(file.filename)
 open("C:/xampp/htdocs/Now_really_working_email_sender/tem/"+ filenames, "wb").write(file.file.read())
 
-email = outlook.CreateItem(0)
+titulo = f"Olá meu nome é {nome}"
+
 
 email.To = "mamacosupremo.sol@gmail.com"
-email.Subject = f"Olá meu nome é {nome}"
+email.Subject = titulo
 email.HTMLbody = (f"""
-                    <h1 style="background: #222; padding: 20px; border-radius: 20px;"> Olá Rafael!</h1>
+                    <h1> Olá Rafael!</h1>
                     
                     <p>{Text}</p>
-                    
+                    <img scr='tem/{filenames}'>
                     <h2> De: </h2> <h1>{nome}</h1>
                     """)
 email.Send()
-print("Email Enviado")
